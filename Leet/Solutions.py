4d4907dcd6bc95f7860ca7a11636632af853ced3
MeheretAl <meheretxo4@gmail.com>
2023-08-15 23:48:52 +0300

    longest common prefix problem

--- Leet/Solutions.py
+++ Leet/Solutions.py
@@ -19,3 +19,16 @@ class Solutions:
                 right = mid
             else:
                 return mid
+            
+    def longestCommonPrefix(self, strs:list[str]) -> str:
+        res = ""
+        prefix = strs[0]
+        for s in strs:
+            prefix = prefix if len(prefix) < len(s) else s
+        
+        for i in range(len(prefix)):
+            for s in strs:
+                if i == len(s) or s[i] != prefix[i]:
+                    return res
+            res += strs[0][i]
+        return res
