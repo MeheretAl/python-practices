146b25f221e9929168d64e89798eb071f4a32e4e
MeheretAl <meheretxo4@gmail.com>
2023-08-20 15:19:56 +0300

    leet code binary search

--- Leet/Solutions.py
+++ Leet/Solutions.py
@@ -55,3 +55,31 @@ class Solutions:
         #there is a better way to do this with binary search on the 2D matrix itself
         #i just converted the 2D matrix into a one dimensional array then searched through that list
         return False if target not in arr else True
+    
+    def searchRange(self,nums:list[int],target:int) -> list[int]:
+        left_most,right_most = -1,-1
+        left,right = 0, len(nums) - 1
+        while (left <= right):
+            mid = (left+right)//2
+            if nums[mid]==target:
+                left_most = mid
+                right = mid -1
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid +1
+
+        if left_most == -1: return [-1,-1]
+
+        left,right = 0,len(nums) -1
+        while (left<=right):
+            mid =(left+right)//2
+            if nums[mid] == target:
+                right_most = mid
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+
+        return [left_most,right_most] if right_most != -1 else [-1,-1]
