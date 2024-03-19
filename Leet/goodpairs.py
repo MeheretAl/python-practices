38274377eed327b600b6243b56de454bec3f4e3c
MeheretAl <meheretxo4@gmail.com>
2024-03-20 02:06:45 +0300

    added goodpairs.py /optimized

---
+++ Leet/goodpairs.py
@@ -0,0 +1,11 @@
+def goodpairs(nums:list[int]) -> int:
+    dict1 = {}
+    counter = 0
+    for i in range(len(nums)):
+        if nums[i] in dict1:
+            counter += dict1[nums[i]]
+            dict1[nums[i]] += 1
+        else:
+            dict1[nums[i]] = 1
+
+    return counter
