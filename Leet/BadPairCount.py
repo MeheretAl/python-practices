7be52f576af6a410878043afd35428dc9dd0729b
MeheretAl <meheretxo4@gmail.com>
2024-03-20 12:28:28 +0300

    removed enumerate

--- Leet/BadPairCount.py
+++ Leet/BadPairCount.py
@@ -4,7 +4,9 @@ from collections import defaultdict
 def badPairCount(nums: list[int]) -> int:
     goodCounter = 0
     dict1 = defaultdict(int)
-
+    for i in range(len(nums)):
+        goodCounter += dict1[nums[i] - i]
+        dict1[nums[i] - i] += 1
     size = len(nums)
     totalPairs = int((size * (size - 1)) / 2)
     return totalPairs - goodCounter
