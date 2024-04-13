4cc673322c34dc938365c4188162c727f8bbfc73
MeheretAl <meheretxo4@gmail.com>
2024-04-13 22:40:53 +0300

    did it using binary search

--- Leet/searchinsert.py
+++ Leet/searchinsert.py
@@ -1,7 +1,13 @@
-def searchInsert(nums: list[int],target:int) -> int: 
-        if target in nums:
-            return nums.index(target)
+def searchInsert(self, nums: list[int], target: int) -> int:
+    if target in nums:
+        return nums.index(target)
+
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] < target:
+            left = mid + 1
         else:
-            nums.append(target)
-            nums.sort()
-            return nums.index(target)
+            right = mid - 1
+
+    return left
