c54eb5ccea84e65ce2eb29af8992fb9b41b1c580
MeheretAl <meheretxo4@gmail.com>
2024-04-13 22:09:13 +0300

    formatting changed

--- Leet/spiralMatrix.py
+++ Leet/spiralMatrix.py
@@ -1,42 +1,40 @@
-def spiralMatrix(matrix : list[list[int]]) -> list[int]:
-    left,right,top,bottom = 0,len(matrix[0]),0,len(matrix)
+def spiralMatrix(matrix: list[list[int]]) -> list[int]:
+    left, right, top, bottom = 0, len(matrix[0]), 0, len(matrix)
     '''
     [1,2,3]
     [4,5,6]
     [7,8,9]
     '''
-    seen = set()
+
     answer = []
     while left < right and top < bottom:
-        #from top left to top right
-        for i in range(left,right):
+        # from top left to top right
+        for i in range(left, right):
             answer.append(matrix[top][i])
-            
         top += 1
-        #from top to bottom
-        for i in range(top,bottom):
-            answer.append(matrix[i][right-1])
-            
+
+        # from top to bottom
+        for i in range(top, bottom):
+            answer.append(matrix[i][right - 1])
         right -= 1
 
-        if not(left<right and top < bottom):
+        if not (left < right and top < bottom):
             break
 
-        #from bottom right to bottom left 
-        for i in range(right-1,left-1,-1):
-            answer.append(matrix[bottom-1][i])
-            
+        # from bottom right to bottom left
+        for i in range(right - 1, left - 1, -1):
+            answer.append(matrix[bottom - 1][i])
         bottom -= 1
-        #from bottom to top
-        for i in range(bottom-1,top-1,-1):
+        # from bottom to top
+        for i in range(bottom - 1, top - 1, -1):
             answer.append(matrix[i][left])
-            
-        left+=1
 
-    print(seen)
+        left += 1
+
     return answer
 
-mat = [[1,2,3,4],
-       [5,6,7,8],
-       [7,8,9,10]]
+
+mat = [[1, 2, 3, 4],
+       [5, 6, 7, 8],
+       [7, 8, 9, 10]]
 print(spiralMatrix(mat))
